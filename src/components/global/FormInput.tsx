1fa1af5b165ea4438bcf0f9c9916bd4a18e749b4
Arman <119550818+theaiemperor@users.noreply.github.com>
2025-04-27 18:23:58 +0530

    Added Validations using zod

--- src/components/global/FormInput.tsx
+++ src/components/global/FormInput.tsx
@@ -1,99 +1,40 @@
-import {
-  Control,
-  Controller,
-  ErrorOption,
-  FieldValues,
-  Path,
-  RegisterOptions,
-} from "react-hook-form";
+import React from "react";
+import { useController } from "react-hook-form";
 import { TextInputProps } from "react-native";
-import { Box } from "../ui/box";
+import {
+  FormControl,
+  FormControlError,
+  FormControlErrorText,
+} from "../ui/form-control";
 import { Input, InputField } from "../ui/input";
-import { Text } from "../ui/text";
 
-interface Props<T extends FieldValues> extends TextInputProps {
-  control: Control<T>;
-  name: Path<T>;
-  title?: boolean | string;
-  errors?: ErrorOption;
-  isNumber?: boolean;
-  isEmail?: boolean;
-  hidePlaceHolder?: boolean;
-  validation?:
-    | Omit<
-        RegisterOptions<T, Path<T>>,
-        "valueAsNumber" | "valueAsDate" | "setValueAs" | "disabled"
-      >
-    | undefined;
+interface Props extends TextInputProps {
+  name: string;
+  label: string;
+  placeholder?: string;
+  containerClassName?: string;
 }
-
-export default function FormInput<T extends FieldValues>(props: Props<T>) {
+export default function ({ name, label, containerClassName, ...props }: Props) {
   const {
-    control,
-    name,
-    title,
-    errors,
-    hidePlaceHolder,
-    validation,
-    isEmail,
-    isNumber,
-    ...rest
-  } = props;
+    field: { onBlur, value, onChange },
+    fieldState: { error },
+  } = useController({ name });
+
   return (
-    <Box>
-      {title && (
-        <Text className="text-pretty text-sm pl-0.5">
-          {typeof title === "string" ? title : name}
-        </Text>
-      )}
-      <Controller
-        control={control}
-        name={name}
-        rules={{
-          ...validation,
-          ...(isNumber && {
-            pattern: {
-              value: /^[0-9]+$/,
-              message: "Only numeric values are allowed",
-            },
-          }),
-          ...(isEmail && {
-            pattern: {
-              value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-              message: "Please enter a valid email address",
-            },
-          }),
-        }}
-        render={({ field: { value, onChange, onBlur } }) => (
-          <Box>
-            <Input>
-              <InputField
-                {...{
-                  ...rest,
-                  placeholder: hidePlaceHolder
-                    ? undefined
-                    : rest.placeholder || name,
-                }}
-                value={value}
-                onBlur={onBlur}
-                onChangeText={onChange}
-              />
-            </Input>
-          </Box>
-        )}
-      />
-      {errors && (
-        <Box>
-          <Text className="text-error-700 text-xs mt-0.5">
-            {errors.type === "required" && `${name} is required`}
-            {errors.type === "maxLength" && `${name} must be shorter`}
-            {errors.type === "minLength" && `${name} must be longer`}
-            {errors.type === "max" && "Enter some small value"}
-            {errors.type === "min" && "Enter some big value"}
-            {errors.message}
-          </Text>
-        </Box>
-      )}
-    </Box>
+    <FormControl isInvalid={error ? true : false}>
+      <Input className={containerClassName}>
+        <InputField
+          {...props}
+          placeholder={props.placeholder || label}
+          onChangeText={onChange}
+          onBlur={onBlur}
+          value={value || ""}
+        />
+      </Input>
+
+      <FormControlError>
+        <FormControlErrorText>{error?.message}</FormControlErrorText>
+      </FormControlError>
+    </FormControl>
   );
 }
--- src/components/routes/invoice/InvoiceForm.tsx
+++ src/components/routes/invoice/InvoiceForm.tsx
@@ -1,4 +1,6 @@
-import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { FormProvider, useForm } from "react-hook-form";
+import { z } from "zod";
 import FormInput from "../../global/FormInput";
 import { Button, ButtonText } from "../../ui/button";
 import { Card } from "../../ui/card";
@@ -6,45 +8,43 @@ import { Heading } from "../../ui/heading";
 import { VStack } from "../../ui/vstack";
 
 export default function InvoiceForm() {
-  const defaultValues = {
-    Title: "",
-    Address: "",
-    "Tax ID": "",
-  };
+  const senderInfoSchema = z.object({
+    name: z
+      .string({ required_error: "Name is Required" })
+      .min(3, { message: "Minimum length will be 3." }),
+    address: z
+      .string({ required_error: "Address is Required." })
+      .min(3, { message: "Minimum length will be 3." }),
+    taxID: z.string().optional(),
+  });
 
-  const {
-    control,
-    handleSubmit,
-    formState: { errors },
-  } = useForm({ defaultValues });
+  type SenderInfo = z.infer<typeof senderInfoSchema>;
 
-  function onSubmit(data: typeof defaultValues) {
+  const form = useForm<SenderInfo>({
+    resolver: zodResolver(senderInfoSchema),
+  });
+
+  function onSubmit(data: any) {
     console.log(data);
   }
 
   return (
     <Card className="w-96">
       <VStack className="gap-5">
-        <Heading className="text-center">New Invoice</Heading>
-        <FormInput
-          control={control}
-          validation={{ required: true }}
-          name="Title"
-          errors={errors.Title}
-        />
-        <FormInput
-          control={control}
-          validation={{ required: true }}
-          name="Address"
-          errors={errors.Address}
-        />
-        <FormInput
-          control={control}
-          validation={{ required: true }}
-          name="Tax ID"
-          errors={errors["Tax ID"]}
-        />
-        <Button onPress={handleSubmit(onSubmit)}>
+        <FormProvider {...form}>
+          <Heading className="text-center">New Invoice</Heading>
+
+          <FormInput name="name" label="Name" />
+          <FormInput
+            name="address"
+            label="Address"
+            multiline
+            containerClassName="h-20 pt-2"
+          />
+          <FormInput name="taxID" label="Tax ID" />
+        </FormProvider>
+
+        <Button onPress={form.handleSubmit(onSubmit)}>
           <ButtonText>Submit</ButtonText>
         </Button>
       </VStack>
